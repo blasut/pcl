b33e562fb6b4578d6dd16042e997005844b3cb40
oklaridentitet <christopher.schmolzer@gmail.com>
2014-10-08 09:30:22 +0200

    ignore everything

--- chapter24.lisp
+++ chapter24.lisp
@@ -23,6 +23,8 @@
         ,(let (,@(loop for n in names for g in gensyms collect `(,n ,g)))
            ,@body)))))
 
+(defvar *in-progress-objects* nil)
+
 (defgeneric read-value (type stream &key)
   (:documentation "Read a value of the given type from the stream."))
 
@@ -43,10 +45,10 @@
   (let ((name (first spec)))
     `(,name :initarg ,(as-keyword name) :accessor ,name)))
 
+(defun mklist (x) (if (listp x) x (list x)))
+
 (defun normalize-slot-spec (spec)
   (list (first spec) (mklist (second spec))))
-
-(defun mklist (x) (if (listp x) x (list x)))
 
 (defun slot->write-value (spec stream)
   (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
@@ -65,28 +67,104 @@
   (assert (typep value type))
   (write-object value stream))
 
-(defmacro define-binary-class (name superclasses slots)
+(defun direct-slots (name)
+  (copy-list (get name 'slots)))
+
+(defun inherited-slots (name)
+  (loop for super in (get name 'superclasses)
+       nconc (direct-slots super)
+       nconc (inherited-slots super)))
+
+(defun all-slots (name)
+  (nconc (direct-slots name) (inherited-slots name)))
+
+(defun new-class-all-slots (slots superclasses)
+  (nconc (mapcan #'all-slots superclasses) (mapcar #'first slots)))
+
+(defun slot->binding (spec stream)
+  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+    `(,name (read-value ',type ,stream ,@args))))
+
+(defun slot->keyword-arg (spec)
+  (let ((name (first spec)))
+    `(,(as-keyword name) ,name)))
+
+(defmethod read-object :around (object stream)
+  (declare (ignore stream))
+  (let ((*in-progress-objects* (cons object *in-progress-objects*)))
+    (call-next-method)))
+
+(defmethod write-object :around (object stream)
+  (declare (ignore stream))
+  (let ((*in-progress-objects* (cons object *in-progress-objects*)))
+    (call-next-method)))
+
+(defun current-binary-object () (first *in-progress-objects*))
+
+(defun parent-of-type (type)
+  (find-if #'(lambda (x) (typep x type)) *in-progress-objects*))
+
+(defmacro define-generic-binary-class (name (&rest superclasses) slots read-method)
   (with-gensyms (objectvar streamvar)
     `(progn
+       (eval-when (:compile-toplevel :load-toplevel :execute)
+	 (setf (get ',name 'slots) ',(mapcar #'first slots))
+	 (setf (get ',name 'superclasses) ',superclasses))
+
        (defclass ,name ,superclasses ()
 	 ,(mapcar #'slot->defclass-slot slots))
       
-       (defmethod read-object progn ((,objectvar ,name) ,streamvar)
-	 (with-slots ,(mapcar #'first slots) ,objectvar
-	   ,@(mapcar #'(lambda (x) (slot->read-value x streamvar)) slots)))
-       
+       ,read-method
+ 
        (defmethod write-object progn ((,objectvar ,name) ,streamvar)
-	 (with-slots ,(mapcar #'first slots) ,objectvar
+	 (declare (ignorable ,streamvar))
+	 (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
 	   ,@(mapcar #'(lambda (x) (slot->write-value x streamvar)) slots))))))
 
 
+(defmacro define-binary-class (name (&rest superclasses) slots)
+  (with-gensyms (objectvar streamvar)
+    `(define-generic-binary-class ,name ,superclasses ,slots
+       (defmethod read-object progn ((,objectvar ,name) ,streamvar)
+	 (declare (ignorable ,streamvar))
+	 (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
+	   ,@(mapcar #'(lambda (x) (slot->read-value x streamvar)) slots))))))
 
 
+(defmacro define-tagged-binary-class (name (&rest superclasses) slots &rest options)
+  (with-gensyms (typevar objectvar streamvar)
+    `(define-generic-binary-class ,name ,superclasses ,slots
+       (defmethod read-value ((,typevar (eql ',name)) ,streamvar &key)
+	 (let* ,(mapcar #'(lambda (x) (slot->binding x streamvar)) slots)
+	   (let ((,objectvar
+		  (make-instance
+		   ,@(or (cdr (assoc :dispatch options))
+			 (error "Must supply :dispatch form."))
+		   ,@(mapcan #'slot->keyword-arg slots))))
+	     (read-object ,objectvar ,streamvar)
+	     ,objectvar))))))
 
 
-
-
-
+(defmacro define-binary-type (name (&rest args) &body spec)
+  (ecase (length spec)
+    (1
+     (with-gensyms (type stream value)
+       (destructuring-bind (derived-from &rest derived-args) (mklist (first spec))
+	 `(progn
+	    (defmethod read-value ((,type (eql ',name)) ,stream &key ,@args)
+	      (read-value ',derived-from ,stream ,@derived-args))
+	    (defmethod write-value ((,type (eql ',name)) ,stream ,value &key ,@args)00
+	      (write-value ',derived-from ,stream ,value ,@derived-args))))))
+     
+     (2
+      (with-gensyms (type)
+	`(progn
+	   ,(destructuring-bind ((in) &body body) (rest (assoc :reader spec))
+	      `(defmethod read-value ((,type (eql ',name)) ,in &key ,@args)
+		 ,@body))
+	   ,(destructuring-bind ((out value) &body body) (rest (assoc :write spec))
+	      `(defmethod write-value ((,type (eql ',name)) ,out ,value &key ,@args)
+		 ,@body)))))))
 
 
 
