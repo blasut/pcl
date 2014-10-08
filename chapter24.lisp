98b58ff1e698171b343190b4364de363daa18a4e
oklaridentitet <christopher.schmolzer@gmail.com>
2014-10-08 08:28:15 +0200

    use read/write object

--- chapter24.lisp
+++ chapter24.lisp
@@ -29,16 +29,19 @@
 (defgeneric write-value (type stream value &key)
   (:documentation "Write a value as the given type to the stream."))
 
+(defgeneric read-object (object stream)
+  (:method-combination progn :most-specific-last)
+  (:documentation "Fill in the slots of object from stream."))
+
+(defgeneric write-object (object stream)
+  (:method-combination progn :most-specific-last)
+  (:documentation "Write out the slots of object to the stream."))
 
 (defun as-keyword (sym) (intern (string sym) :keyword))
 
 (defun slot->defclass-slot (spec)
   (let ((name (first spec)))
     `(,name :initarg ,(as-keyword name) :accessor ,name)))
-
-(defun slot->read-value (spec stream)
-  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
-    `(setf ,name (read-value ',type, stream ,@args))))
 
 (defun normalize-slot-spec (spec)
   (list (first spec) (mklist (second spec))))
@@ -49,22 +52,32 @@
   (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
     `(write-value ',type ,stream ,name ,@args)))
 
-(defmacro define-binary-class (name slots)
-  (with-gensyms (typevar objectvar streamvar)
+(defun slot->read-value (spec stream)
+  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+    `(setf ,name (read-value ',type, stream ,@args))))
+
+(defmethod read-value ((type symbol) stream &key)
+  (let ((object (make-instance type)))
+    (read-object object stream)
+    object))
+
+(defmethod write-value ((type symbol) stream value &key)
+  (assert (typep value type))
+  (write-object value stream))
+
+(defmacro define-binary-class (name superclasses slots)
+  (with-gensyms (objectvar streamvar)
     `(progn
-       (defclass ,name ()
+       (defclass ,name ,superclasses ()
 	 ,(mapcar #'slot->defclass-slot slots))
       
-       (defmethod read-value ((,typevar (eql ',name)) ,streamvar &key)
-	 (let ((,objectvar (make-instance ',name)))
-	   (with-slots ,(mapcar #'first slots) ,objectvar
-	     ,@(mapcar #'(lambda (x) (slot->read-value x streamvar)) slots))
-	   ,objectvar))
-
-       (defmethod write-value ((,typevar (eql ',name)) ,streamvar ,objectvar &key)
+       (defmethod read-object progn ((,objectvar ,name) ,streamvar)
 	 (with-slots ,(mapcar #'first slots) ,objectvar
-	   ,@(mapcar #'(lambda (x) (slot->write-value x streamvar)) slots)))))))))
-
+	   ,@(mapcar #'(lambda (x) (slot->read-value x streamvar)) slots)))
+       
+       (defmethod write-object progn ((,objectvar ,name) ,streamvar)
+	 (with-slots ,(mapcar #'first slots) ,objectvar
+	   ,@(mapcar #'(lambda (x) (slot->write-value x streamvar)) slots))))))
 
 
 
