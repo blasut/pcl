265b36150852384f614ca746fe25d45af5f50b56
oklaridentitet <oklaridentitet@gmail.com>
2014-10-14 10:47:33 +0200

    match stuffs

--- chapter27/mp3-database.lisp
+++ chapter27/mp3-database.lisp
@@ -165,8 +165,70 @@
      :test #'mp3-p)
     (format t "~&Loaded ~d files into database." count)))
 
+(defun extractor (schema)
+  (let ((names (mapcar #'name schema)))
+    #'(lambda (row)
+	(loop :for c :in names :collect c :collect (getf row c)))))
 
+(defun row-equality-tester (schema)
+  (let ((names (mapcar #'name schema))
+	(tests (mapcar #'equality-predicate schema)))
+    #'(lambda (a b)
+	(loop :for name :in names :and test :in tests
+	   :always (funcall test (getf a name) (getf b name))))))
 
+(defun row-comparator (column-names schema)
+  (let ((comparators (mapcar #'comparator (extract-schema column-names schema))))
+    #'(lambda (a b)
+	(loop
+	   :for name :in column-names
+	   :for comparator :in comparators
+	   :for a-value = (getf a name)
+	   :for b-value = (getf b name)
+	   :when (funcall comparator a-value b-value) return t
+	   :when (funcall comparator b-value a-value) return nil
+	   :finally (return nil)))))
+
+(defun mklist (thing)
+  (if (listp thing) thing (list thing)))
+
+(defun find-column (column-name schema)
+  (or (find column-name schema :key #'name)
+      (error "No column: ~a in schema: ~a" column-name schema)))
+
+(defun restrict-rows (rows where)
+  (remove-if-not where rows))
+
+(defun project-columns (rows schema)
+  (map 'vector (extractor schema) rows))
+
+(defun distinct-rows (rows schema)
+  (remove-duplicates rows :test (row-equality-tester schema)))
+
+(defun sorted-rows (rows schema order-by)
+  (sort (copy-seq rows) (row-comparator order-by schema)))
+
+(defun extract-schema (column-names schema)
+  (loop :for c :in column-names :collect (find-column c schema)))
+
+(defun select (&key (columns t) from where distinct order-by)
+  (let ((rows (rows from))
+	(schema (schema from)))
+    
+    (when where
+      (setf rows (restrict-rows rows where)))
+    
+    (unless (eql columns 't)
+      (setf schema (extract-schema (mklist columns) schema))
+      (setf rows (project-columns rows schema)))
+    
+    (when distinct
+      (setf rows (distinct-rows rows schema)))
+    
+    (when order-by
+      (setf rows (sorted-rows rows schema (mklist order-by))))
+    
+    (make-instance 'table :rows rows :schema schema)))
 
 
 
