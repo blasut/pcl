29d09de0cc0ceccd326eae38511415116f23a396
oklaridentitet <oklaridentitet@gmail.com>
2014-10-16 10:19:44 +0200

    done with c31

--- chapter30.lisp
+++ chapter30.lisp
@@ -7,69 +7,55 @@
            :emit-html
            :&attributes))
 
+(defparameter *pretty* t)
+(defparameter *html-output* *standard-output*)
+(defvar *html-pretty-printer* nil)
+
+(defparameter *xhtml* nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Public API
+
+(defun emit-html (sexp) (process (get-pretty-printer) sexp))
+
+(defmacro in-html-style (syntax)
+  (eval-when (:compile-toplevel :load-toplevel :execute)
+    (case syntax
+      (:html (setf *xhtml* nil))
+      (:xhtml (setf *xhtml* t)))))
+
+(defmacro with-html-output ((stream &key (pretty *pretty*)) &body body)
+  `(let* ((*html-output* ,stream)
+	  (*pretty* ,pretty))
+     ,@body))
+
+(defmacro html (&whole whole &body body)
+  (declare (ignore body))
+  `(if *pretty*
+       (macrolet ((html (&body body) (codegen-html (sexp->ops body) t)))
+	 (let ((*html-pretty-printer* (get-pretty-printer))) ,whole))
+       (macrolet ((html (&body body) (codegen-html (sexp->ops body) nil)))
+	 ,whole)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Helpers for public API
+
+(defun codegen-html (ops pretty)
+  (let ((*pretty* pretty))
+    `(prognou ,@(generate-code (optimize-static-output ops)) nil)))
+
+(defun get-pretty-printer ()
+  (or *html-pretty-printer*
+      (make-instance
+       'html-pretty-printer
+       :printer (make-instance 'indenting-printer :out *html-output*))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; String escaping
+
 (defparameter *element-escapes* "<>&")
 (defparameter *attribute-escapes* "<>&\"'")
 (defparameter *escapes* *element-escapes*)
-
-
-;;;; Elements predicates
-
-(defparameter *block-elements*
-  '(:body :colgroup :dl :fieldset :form :head :html :map :noscript :object
-    :ol :optgroup :pre :script :select :style :table :tbody :tfoot :thead
-    :tr :ul))
-
-(defparameter *paragraph-elements*
-  '(:area :base :blockquote :br :button :caption :col :dd :div :dt :h1
-    :h2 :h3 :h4 :h5 :h6 :hr :input :li :link :meta :option :p :param
-    :td :textarea :th :title))
-
-(defparameter *inline-elements*
-  '(:a :abbr :acronym :address :b :bdo :big :cite :code :del :dfn :em
-    :i :img :ins :kbd :label :legend :q :samp :small :span :strong :sub
-    :sup :tt :var))
-
-(defparameter *empty-elements*
-  '(:area :base :br :col :hr :img :input :link :meta :param))
-
-(defparameter *preserve-whitespace-elements* '(:pre :script :style))
-(defparameter *xhtml* nil)
-
-(defun block-element-p (tag) (find tag *block-elements*))
-(defun paragraph-element-p (tag) (find tag *paragraph-elements*))
-(defun empty-element-p (tag) (find tag *empty-elements*))
-(defun preserve-whitespace-p (tag) (find tag *preserve-whitespace-elements*))
-
-;;;;  Stuff
-
-(defun self-evaluating-p (form)
-  (and (atom form) (if (symbolp form) (keywordp form) t)))
-
-
-(defun cons-form-p (form &optional (test #'keywordp))
-  (and (consp form)
-       (or (funcall test (car form))
-	   (and (consp (car form)) (funcall test (caar form))))))
-
-(defun parse-explicit-attributes-sexp (sexp)
-  (destructuring-bind ((tag &rest attributes) &body body) sexp
-    (values tag attributes body)))
-
-(defun parse-implicit-attributes-sexp (sexp)
-  (loop :with tag = (first sexp)
-     :for rest :on (rest sexp) by #'cddr
-     :while (and (keywordp (first rest)) (second rest))
-     :when (second rest)
-        :collect (first rest) :into attributes :and
-        :collect (second rest) :into attributes
-     :end
-     :finally (return (values tag attributes rest))))
-
-(defun parse-cons-form (sexp)
-  (if (consp (first sexp))
-      (parse-explicit-attributes-sexp sexp)
-      (parse-implicit-attributes-sexp sexp)))
-
 
 (defun escape-char (char)
   (case char
@@ -90,8 +76,11 @@
 	    :when pos :do (write-sequence (escape-char (char in pos)) out)
 	    :while pos))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Indenting printer
+
 (defclass indenting-printer ()
-  ((out                 :accessor out                 :initarg out)
+  ((out                 :accessor out                 :initarg :out)
    (beginning-of-line-p :accessor beginning-of-line-p :initform t)
    (indention           :accessor indention           :initform 0)
    (indenting-p         :accessor indenting-p         :initform t)))
@@ -121,8 +110,8 @@
      :when pos :do (emit-newline ip)
      :while pos))
 
-
-;;;;; Backend interace
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; html processor interace
 
 (defgeneric raw-string (processor string &optional newlines-p))
 
@@ -140,7 +129,8 @@
 
 (defgeneric embed-code (processor code))
 
-;;;;; Pretty printer backend implementation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Html pretty printer
 
 (defclass html-pretty-printer ()
   ((printer    :accessor printer   :initarg :printer)
@@ -160,11 +150,11 @@
 
 (defmethod indent ((pp html-pretty-printer))
   (when *pretty*
-    (incf (indentation (printer pp)) (tab-width pp))))
+    (incf (indention (printer pp)) (tab-width pp))))
 
 (defmethod unindent ((pp html-pretty-printer))
   (when *pretty*
-    (decf (indentation (printer pp)) (tab-width pp))))
+    (decf (indention (printer pp)) (tab-width pp))))
 
 (defmethod toggle-indenting ((pp html-pretty-printer))
   (when *pretty*
@@ -177,11 +167,193 @@
 (defmethod embed-code ((pp html-pretty-printer) code)
   (error "Can't embed code when interpreting. Code: ~s" code))
 
-;;;; Process
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Ops buffer
 
+(defun make-op-buffer () (make-array 10 :adjustable t :fill-pointer 0))
+
+(defun push-op (op ops-buffer) (vector-push-extend op ops-buffer))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Compiler
+
+(defclass html-compiler ()
+  ((ops :accessor ops :initform (make-op-buffer))))
+
+(defmethod raw-string ((compiler html-compiler) string &optional newlines-p)
+  (push-op `(:raw-string ,string ,newlines-p) (ops compiler)))
+
+(defmethod newline ((compiler html-compiler))
+  (push-op '(:newline) (ops compiler)))
+
+(defmethod freshline ((compiler html-compiler))
+  (push-op '(:freshline) (ops compiler)))
+
+(defmethod indent ((compiler html-compiler))
+  (push-op `(:indent) (ops compiler)))
+
+(defmethod unindent ((compiler html-compiler))
+  (push-op `(:unindent) (ops compiler)))
+
+(defmethod toggle-indenting ((compiler html-compiler))
+  (push-op `(:toggle-indenting) (ops compiler)))
+
+(defmethod embed-value ((compiler html-compiler) value)
+  (push-op `(:embed-value ,value ,*escapes*) (ops compiler)))
+
+(defmethod embed-code ((compiler html-compiler) code)
+  (push-op `(:embed-code ,code) (ops compiler)))
+
+(defun generate-code (ops)
+  (loop :for op :across ops :collect (apply #'op->code op)))
+
+(defun sexp->ops (body)
+  (loop :with compiler = (make-instance 'html-compiler)
+     :for form :in body :do (process compiler form)
+     :finally (return (ops compiler))))
+
+(defun compile-buffer (buf ops)
+  (loop :with str = (get-output-stream-string buf)
+     :for start = 0 :then (1+ pos)
+     :for pos = (position #\Newline str :start start)
+     :when (< start (length str))
+     :do (push-op `(:raw-string ,(subseq str start pos) nil) ops)
+     :when pos :do (push-op '(:newline) ops)
+     :while pos))
+
+(defun optimize-static-output (ops)
+  (let ((new-ops (make-op-buffer)))
+    (with-output-to-string (buf)
+      (flet ((add-op (op)
+	       (compile-buffer buf new-ops)
+	       (push-op op new-ops)))
+	(loop :for op :across ops :do
+	   (ecase (first op)
+	     (:raw-string (write-sequence (second op) buf))
+	     ((:newline :embed-value :embed-code) (add-op op))
+	     ((:indent :unindent :freshline :toggle-indenting)
+	      (when *pretty* (add-op op)))))
+	(compile-buffer buf new-ops)))
+    new-ops))
+
+(defgeneric op->code (op &rest operands))
+
+(defmethod op->code ((op (eql :raw-string)) &rest operands)
+  (destructuring-bind (string check-for-newlines) operands
+    (if *pretty*
+	`(raw-string *html-pretty-printer* ,string ,check-for-newlines)
+	`(write-sequence ,string *html-output*))))
+
+(defmethod op->code ((op (eql :newline)) &rest operands)
+  (if *pretty*
+      `(newline *html-pretty-printer*)
+      `(write-char #\Newline *html-output*)))
+
+(defmethod op->code ((op (eql :freshline)) &rest operands)
+  (if *pretty*
+      `(freshline *html-pretty-printer*)
+      (error "Bad op when not pretty-printing: ~a" op)))
+
+(defmethod op->code ((op (eql :indent)) &rest operands)
+  (if *pretty*
+      `(indent *html-pretty-printer*)
+      (error "bad op when not pretty-printing: ~a" op)))
+
+(defmethod op->code ((op (eql :unindent)) &rest operands)
+  (if *pretty*
+      `(unindent *html-pretty-printer*)
+      (error "Bad op when not pretty-printing: ~a" op)))
+
+(defmethod op->code ((op (eql :toggle-indenting)) &rest operands)
+  (if *pretty*
+      `(toggle-indenting *html-pretty-printer*)
+      (error "Bad op when not pretty printing: ~a" op)))
+
+(defmethod op->code ((op (eql :embed-value)) &rest operands)
+  (destructuring-bind (value escapes) operands
+    (if *pretty*
+	(if escapes
+	    `(raw-string *html-pretty-printer* (escape (princ-to-string ,value) ,escapes) t)
+	    `(raw-string *html-pretty-printer* (princ-to-string ,value) t))
+	(if escapes
+	    `(write-sequence (escape (princ-to-string ,value) ,escapes) *html-output*)
+	    `(princ ,value *html-output*)))))
+
+(defmethod op->code ((op (eql :embed-code)) &rest operands)
+  (first operands))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; HTML Processor
+
+(defun process (processor form)
+  (cond
+    ((special-form-p form) (process-special-form processor form))
+    ((macro-form-p form)   (process processor (expand-macro-form form)))
+    ((sexp-html-p form)    (process-sexp-html processor form))
+    ((consp form)          (embed-code processor form))
+    (t                     (embed-value processor form))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Language syntax
+
+(defun self-evaluating-p (form)
+  (and (atom form) (if (symbolp form) (keywordp form) t)))
 
 (defun sexp-html-p (form)
   (or (self-evaluating-p form) (cons-form-p form)))
+
+(defun special-form-p (form)
+  (and (consp form) (symbolp (car form)) (get (car form) 'html-special-operator)))
+
+(defun cons-form-p (form &optional (test #'keywordp))
+  (and (consp form)
+       (or (funcall test (car form))
+	   (and (consp (car form)) (funcall test (caar form))))))
+
+(defun parse-explicit-attributes-sexp (sexp)
+  (destructuring-bind ((tag &rest attributes) &body body) sexp
+    (values tag attributes body)))
+
+(defun parse-implicit-attributes-sexp (sexp)
+  (loop :with tag = (first sexp)
+     :for rest :on (rest sexp) by #'cddr
+     :while (and (keywordp (first rest)) (second rest))
+     :when (second rest)
+        :collect (first rest) :into attributes :and
+        :collect (second rest) :into attributes
+     :end
+     :finally (return (values tag attributes rest))))
+
+(defun parse-cons-form (sexp)
+  (if (consp (first sexp))
+      (parse-explicit-attributes-sexp sexp)
+      (parse-implicit-attributes-sexp sexp)))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; SEXP-HTML
+
+(defparameter *block-elements*
+  '(:body :colgroup :dl :fieldset :form :head :html :map :noscript :object
+    :ol :optgroup :pre :script :select :style :table :tbody :tfoot :thead
+    :tr :ul))
+
+(defparameter *paragraph-elements*
+  '(:area :base :blockquote :br :button :caption :col :dd :div :dt :h1
+    :h2 :h3 :h4 :h5 :h6 :hr :input :li :link :meta :option :p :param
+    :td :textarea :th :title))
+
+(defparameter *inline-elements*
+  '(:a :abbr :acronym :address :b :bdo :big :cite :code :del :dfn :em
+    :i :img :ins :kbd :label :legend :q :samp :small :span :strong :sub
+    :sup :tt :var))
+
+(defparameter *empty-elements*
+  '(:area :base :br :col :hr :img :input :link :meta :param))
+
+(defparameter *preserve-whitespace-elements* '(:pre :script :style))
 
 (defun emit-attributes (processor attributes)
   (loop :for (k v) :on attributes :by #'cddr :do
@@ -225,25 +397,91 @@
 (defun process-sexp-html (processor form)
   (if (self-evaluating-p form)
       (raw-string processor (escape (princ-to-string form) *escapes*) t)
-      (process-cons-sexp-html processor form))))
+      (process-cons-sexp-html processor form)))
 
-(defun process (processor form)
-  (if (sexp-html-p form)
-      (process-sexp-html processor form)
-      (error "Mailformed FOO form: ~s" form)))
+(defun block-element-p (tag) (find tag *block-elements*))
+(defun paragraph-element-p (tag) (find tag *paragraph-elements*))
+(defun empty-element-p (tag) (find tag *empty-elements*))
+(defun preserve-whitespace-p (tag) (find tag *preserve-whitespace-elements*))
 
-(defun get-pretty-printer ()
-  (or *html-pretty-printer*
-      (make-instance
-       'html-pretty-printer
-       :printer (make-instance 'indenting-printer :out *html-output*))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Special operators
 
-(defun emit-html (sexp) (process (get-pretty-printer) sexp))
+(defmacro define-html-special-operator (name (processor &rest other-parameters) &body body)
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (setf (get ',name 'html-special-operator)
+	   (lambda (, processor ,@other-parameters) ,@body))))
 
-(defparameter *pretty* nil)
+(defun process-special-form (processor form)
+  (apply (get (car form) 'html-special-operator) processor (rest form)))
 
-(defmacro with-html-output ((stream &key (pretty *pretty*)) &body body)
-  `(let* ((*html-output* ,stream)
-	  (*pretty* ,pretty))
-     ,@body))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Macros
+(defun macro-form-p (form)
+  (cons-form-p form #'(lambda (x) (and (symbolp x) (get x 'html-macro)))))
 
+(defun parse-html-macro-lambda-list (args)
+  (let((attr-cons (member '&attributes args)))
+    (values
+     (cadr attr-cons)
+     (nconc (ldiff args attr-cons) (cddr attr-cons)))))
+
+(defun generate-macro-with-attributes (name attribute-args args body)
+  (with-gensyms (attributes form-body)
+    (if (symbolp attribute-args) (setf attribute-args `(&rest ,attribute-args)))
+    `(eval-when (:compile-toplevel :load-toplevel :execute)
+       (setf (get ',name 'html-macro-wants-attributes) t)
+       (setf (get ',name 'html-macro)
+	     (lambda (,attributes ,form-body)
+	       (destructuring-bind (,@attribute-args) ,attributes
+		 (destructuring-bind (,@args) ,form-body
+		   ,@body)))))))
+
+(defun generate-macro-no-attributes (name args body)
+  (with-gensyms (form-body)
+    `(eval-when (:compile-toplevel :load-toplevel :execute)
+       (setf (get ',name 'html-macro-wants-attributes) nil)
+       (setf (get ',name 'html-macro)
+	     (lambda (,form-body)
+	       (destructuring-bind (,@args) ,form-body ,@body))))))
+
+(defun expand-macro-form (form)
+  (if (or (consp (first form))
+	  (get (first form) 'html-macro-wants-attributes))
+      (multiple-value-bind (tag attributes body) (parse-cons-form form)
+	(funcall (get tag 'html-macro) attributes body))
+      (destructuring-bind (tag &body body) form
+	(funcall (get tag 'html-macro) body))))
+
+(defmacro define-html-macro (name (&rest args) &body body)
+  (multiple-value-bind (attribute-var args)
+      (parse-html-macro-lambda-list args)
+    (if attribute-var
+	(generate-macro-with-attributes name attribute-var args body)
+	(generate-macro-no-attributes name args body))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Special forms
+
+(define-html-special-operator :attribute (processor &rest body)
+  (let ((*escapes* *attribute-escapes*))
+    (loop :for exp :in body :do (process processor exp))))
+
+(define-html-special-operator :print (processor form)
+  (cond
+    ((self-evaluating-p form)
+     (warn "Redundant :print of self-evaluating form ~s" form)
+     (process-sexp-html processor form))
+    (t
+     (embed-value processor form))))
+
+(define-html-special-operator :format (processor &rest args)
+  (if (every  #'self-evaluating-p args)
+      (process-sexp-html processor (apply #'format nil args))
+      (embed-value processor `(format nil ,@args))))
+
+(define-html-special-operator :newline (processor)
+  (newline processor))
+
+(define-html-special-operator :progn (processor &rest body)
+  (loop :for exp :in body :do (process processor exp)))
